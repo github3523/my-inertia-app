9056752b45c5cedbbb6b6d1bff707a5e29dd99e8
github3523 <nathantadesse10@gmail.com>
2026-04-12 19:37:47 +0100

    default homepage changed to react

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,12 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+Route::inertia('/', 'Welcome', [
+    'canLogin' => Route::has('login'),
+    'canRegister' => Route::has('register'),
+    'laravelVersion' => Application::VERSION,
+    'phpVersion' => PHP_VERSION,
+]);
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
